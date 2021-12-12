23603ede90c6fc4bd34514b1be899bcb204b6c26
程序员乐哥 <ml2822797627@163.com>
2021-12-12 22:00:48 +0800

    今天的简单题目

---
+++ 12_12.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//用一个函数实现求两个数的最大值
+//int add(int a, int b)
+//{
+//	if (a > b)
+//		return a;
+//	else
+//		return b;
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//	int c=add(a, b);
+//	printf("最大值为：%d", c);
+//	return 0;
+//}
+//给定两个整数a和b (0 < a,b < 10,000)，计算a除以b的整数商和余数
+//int main()
+//{
+//    int a, b;
+//    scanf("%d%d", &a, &b);
+//    printf("%d %d", a / b, a % b);
+//    return 0;
+//}
