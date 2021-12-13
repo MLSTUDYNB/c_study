3d41d14013745f018d22548525147bce193985a6
程序员乐哥 <ml2822797627@163.com>
2021-12-13 20:56:03 +0800

    三子棋游戏

--- game.c
+++ game.c
@@ -1,111 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "game.h"
-int H, L;
-void csh(char arr[HANG][LIE],char set)
+int h,l;
+void csh(char arr[hang][lie])
 {
-	for (H = 0; H < HANG; H++)
+	for (h = 0; h < hang; h++)
 	{
-		for (L = 0; L < LIE; L++)
+		for (l = 0; l < hang; l++)
 		{
-			arr[H][L] = set;
+			arr[h][l] =' ';
 		}
 	}
 }
-void dayin(char board[HANG][LIE])
+void qipan(char arr[hang][lie])
 {
-	for (L = 0; L <= lie; L++)
+	for (h = 0; h < hang; h++)
 	{
-		printf("%d ", L);
-	}
-	printf("\n");
-	for (H = 1; H <= hang; H++)
-	{
-		printf("%d ", H );
-		for (L = 1; L <= lie; L++)
+		for (l = 0; l < hang; l++)
 		{
-			printf("%c ", board[H][L]);
+			printf(" %c ", arr[h][l]);
+			if (l < hang-1)
+				printf("|");
+		}
+		printf("\n");
+		if (h < hang - 1)
+		{
+			for (l = 0; l < hang; l++)
+			{
+				printf("---");
+				if (l < hang - 1)
+					printf("|");
+			}
 		}
 		printf("\n");
 	}
 }
-void buzhi(char houduan[HANG][LIE])
+void game()
 {
-	int n=LEI_SHU;
-	while (n>0)
-	{
-		H = rand() % hang + 1;
-		L = rand() % lie + 1;
-		if (houduan[H][L] == '0')
-		{
-			houduan[H][L] = '1';
-			n--;
-		}
-	}
-}
-void saolei(char huoduan[HANG][LIE], char yonghu[HANG][LIE])
-{
-	int n = hang*lie-LEI_SHU;
-	while (n>0)
-	{	
-		printf("请输入坐标->");
-		scanf("%d%d", &H, &L);
-		if (H >= 1 && H <= 9 && L >= 1 && L <= 9)
-		{
-			if (huoduan[H][L] == '1')
-			{
-				printf("哈哈，你被炸死了\n");
-				break;
-			}
-			else
-			{
-				PDZW(huoduan,yonghu, H, L);				//判断周围有没有雷
-				system("cls");
-				dayin(yonghu);
-				n--;
-			}
-		}
-		else
-		{
-			printf("请输入正确的坐标->");
-		}
-	}
-	if (n == 0)
-	{
-		printf("恭喜你，扫雷成功\n");
-	}
-}
-void PDZW(char huoduan[HANG][LIE], char yonghu[HANG][LIE], int H, int L)
-{
-	int h = H;
-	int l = L;
-	int a = 0;
-	char sum = '0';
-	//for (h = H - 1; h <= H + 1; h++)
-	//{
-	//	for (l = L - 1; l <= L + 1; l++)
-	//	{
-	//		if (huoduan[h][l] == '0')
-	//		{
-	//			a++;
-	//		}
-	//	}
-	//}
-	//if (a == 9)
-	//{
-	//	PDZW(huoduan, yonghu, h, l);
-	//}
-	for (h = H - 1; h <= H + 1; h++)
-	{
-		for (l = L - 1; l <= L + 1; l++)
-		{
-			if (huoduan[h][l] == '1')
-			{
-				//sum = sum + '1';
-				sum++;
-			}
-		}
-	}
-	if (sum == '0')
-		sum = ' ';
-	yonghu[H][L] = sum;
+	char arr[hang][lie] = { 0};
+	csh(arr);//数组初始化
+	qipan(arr);//初始化棋盘
+	
 }
--- game.h
+++ game.h
@@ -1,15 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-#define hang 9
-#define lie 9
-#define HANG hang+2
-#define LIE lie+2
-#define LEI_SHU 10
-
-void csh(char arr[HANG][LIE], char set);
-void dayin(char board[HANG][LIE]);
-void buzhi(char houduan[HANG][LIE]);
-void saolei(char huoduan[HANG][LIE], char yonghu[HANG][LIE]);
-void PDZW(char huoduan[HANG][LIE], char yonghu[HANG][LIE], int H, int L);
+#define hang 3
+#define lie 3
+void csh(char arr[hang][lie]);
+void qipan(char arr[hang][lie]);
+void game();
