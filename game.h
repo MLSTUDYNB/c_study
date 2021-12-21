c1917cd3c59855c413b316cfd51bb6036a8c7f41
程序员乐哥 <ml2822797627@163.com>
2021-12-21 16:29:35 +0800

    练习题

--- game.h
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include <stdlib.h>
-#define hang 3
-#define lie 3
-void csh(char arr[hang][lie]);
-void qipan(char arr[hang][lie]);
-void game();
