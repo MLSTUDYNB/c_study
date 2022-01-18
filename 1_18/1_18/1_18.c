ef4cf94986d54e98b0d7601f5b2e23e56631016a
程序员乐哥 <ml2822797627@163.com>
2022-01-18 18:22:24 +0800

    今天的练习

--- 1_18/1_18/1_18.c
+++ 1_18/1_18/1_18.c
@@ -1,5 +1,108 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
 #include<stdio.h>
+//实现一个函数，打印乘法口诀表，口诀表的行数和列数自己指定
+//如：输入9，输出9 * 9口诀表，输出12，输出12 * 12的乘法口诀表。
+
+void print(int n)
+{
+	int a,b;
+	for (a = 1; a <= n; a++)
+	{
+		for (b = 1; b <= a; b++)
+		{
+			printf("%d*%d=%-3d  ", b, a, a * b);
+		}
+		printf("\n");
+	}
+
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	print(n);
+	return 0;
+}
+
+//实现一个函数来交换两个整数的内容。
+//void judge(int* a,int* b)
+//{
+//	int i;
+//	i = *a;
+//	*a = *b;
+//	*b = i;
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//	judge(&a,&b);
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+
+
+//实现函数判断year是不是润年。
+//int is_leap_year(int year)
+//{
+//	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	int year;
+//	scanf("%d", &year);
+//	if (is_leap_year(year) == 1)
+//	{
+//		printf("是闰年\n");
+//	}
+//	else
+//	{
+//		printf("不是闰年\n");
+//	}
+//	return 0;
+//}
+
+/*实现一个函数，判断一个数是不是素数。
+
+利用上面实现的函数打印100到200之间的素数。
+
+*/
+
+//#include <math.h>
+//
+//int judge(int n)
+//{
+//	int i = 0;
+//	for (i = 2; i <= sqrt(n); i++)
+//	{
+//		if (n % i == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	for (n = 100; n <= 200; n++)
+//	{
+//		if (judge(n))
+//		{
+//			printf("%d ", n);
+//		}
+//	}
+//	return 0;
+//}
 //int f(int n)
 //{
 //	static int i = 1;
