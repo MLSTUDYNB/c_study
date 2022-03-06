91ba64e3c1e84cd46ab45a10ca87a7e9d5d3012e
程序员乐哥 <ml2822797627@163.com>
2022-03-06 23:00:05 +0800

    编程练习

--- 3_6/3_6/3_6.c
+++ 3_6/3_6/3_6.c
@@ -14,8 +14,8 @@ n%10最后一位数   1剩下146
 n%10       6        14
 n%10      4       1
 求出 5位数中的所有 Lily Number。*/
-#include <stdio.h>
-#include <math.h>
+//#include <stdio.h>
+//#include <math.h>
 //int main()
 //{
 //    int n;
@@ -61,8 +61,90 @@ n%10      4       1
 //		printf("c\n");
 //	return 0;
 //}
+//int main()
+//{
+//	float i = 6.5;
+//	return 0;
+//}
+#include <malloc.h>
+//*
+//* *
+//*   *
+//* * * *
+//
+//*
+//* *
+//*   *
+//*     *
+//* * * * *
+//int main()
+//{
+//	int n;
+//	while (~scanf("%d",&n))
+//	{
+//		char arr[20][20];
+//		int i, j;
+//		for (i=0;i<n;i++)
+//		{
+//			for (j=0;j<=i;j++)
+//			{
+//				arr[i][j] = '*';
+//				if (i >= 2 && j >= 1 && i < n - 1 && j < n - 2&&j!=i)
+//					arr[i][j] = ' ';
+//				printf("%c ", arr[i][j]);
+//			}
+//			printf("\n");	
+//		}
+//	}
+//	return 0;
+//}
+#include <stdio.h>
+
 int main()
 {
-	float i = 6.5;
-	return 0;
+    int n, m;
+    scanf("%d %d", &n, &m);//第一行
+    int arr1[1000] = { 0 };
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr1[i]);//第二行
+    }
+    int arr2[1000] = { 0 };
+    for (j = 0; j < m; j++)
+    {
+        scanf("%d", &arr2[j]);//第三行
+    }
+    i = 0;
+    j = 0;
+    while (i < n &&j < m)//当一个数组遍历完后，剩下数组直接输出即可
+    {
+        while (arr1[i] <= arr2[j]&& i < n && j < m)
+        {
+            printf("%d ", arr1[i]);
+            i++;
+        }
+        while(arr1[i] >= arr2[j] && i < n && j < m)
+        {
+            printf("%d ", arr2[j]);
+            j++;
+        }
+    }
+    //if (i == n)
+    //{
+    //    for (; j < m; j++)
+    //    {
+    //        printf("%d ", arr2[j]);
+    //    }
+    //}
+    //else
+    //{
+    //    for (; i < n; i++)
+    //    {
+    //        printf("%d ", arr1[i]);
+    //    }
+    //}
+
+    return 0;
 }
