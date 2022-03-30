7d301d0ff69f3674691eda0ea4be576861d0a377
程序员乐哥 <ml2822797627@163.com>
2022-03-30 19:11:00 +0800

    库函数

--- 3_24/3_24/3_24.c
+++ 3_24/3_24/3_24.c
@@ -54,7 +54,41 @@ int kmp(const char* str1, const char* str2)
 int main()
 {
 	char arr[] = "abaabcdabcab";
-	char brr[] = "ef";
+	char brr[] = "aaaaaaaad";
 	printf("%d\n",kmp(arr, brr));
 	return 0;
 }
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//	double score;
+//};
+//cmp_name(const void* e1, const void* e2)
+//{
+//	return strcmp(((struct stu*)e1)->name, ((struct stu*)e2)->name);
+//}
+//print(struct stu* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%s,%d,%lf\n", (p + i)->name, (p + i)->age, (p + i)->score);
+//	}
+//}
+//test()
+//{
+//	struct stu arr[3] = { {"zhangshang",20,50.0},{"lisi",10,60.0},{"wangwu",30,70.0} };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz, sizeof(arr[0]), cmp_name);
+//	print(arr, sz);
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+//
