eb4a1778e030959351c8a7d89edae0b19dfae3bd
程序员乐哥 <ml2822797627@163.com>
2022-07-15 19:51:30 +0800

    选择题

--- c++基础/c++基础/test.cpp
+++ c++基础/c++基础/test.cpp
@@ -1,10 +1,81 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <iostream>
-
+#include "test.h"
 using namespace std;
-int main()
-{
-	cout << 0 % 2;
-	return 0;
-}
+//namespace ML
+//{
+//	int a = 10;
+//}
+//int main()
+//{
+//	int a = 1;
+//	cout << a << endl;
+//	cout << ML::a;
+//	return 0;
+//}
+
+//void f(int a = 10, int b = 20)
+//{
+//	printf("a=%d,b=%d", a, b);
+//}
+//int main()
+//{
+//	f();
+//	f(1,2);
+//	return 0;
+//}
+
+//int f(int a, int b)
+//{
+//	cout << a <<' ' << b << endl;
+//	return 0;
+//}
+//int f(int a, char b)
+//{
+//	cout << a <<' ' << b << endl;
+//	return 0;
+//}
+//void f(char a, int b)
+//{
+//	cout << a << ' ' << b << endl;
+//}
+//int main()
+//{
+//	//f(1, 1);
+//	//f(1, 'a');
+//	//f('a', 1);
+//	//int a = 10;
+//	//int& b = a;
+//	//cout << b << endl;
+//	//cout << a << endl;
+//	//cout << &b << endl;
+//	//cout << &a << endl;
+//	return 0;
+//}
+
+//int& f(int a)
+//{
+//	a = 20;
+//	return a;
+//}
+//int main()
+//{
+//	//int& ret=f(10);
+//	//cout << ret << endl;
+//	//cout << ret << endl;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	const int& pa = a;
+//	const double& pa2 = a;
+//	printf("&a=%p\n", &a);
+//	printf("&pa=%p\n", &pa);
+//	printf("&pa2=%p\n", &pa2);
+//
+//	return 0;
+//}
---
+++ 选择题2/选择题2/test.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//#define Mul(x,y) ++x*++y
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = f;
+//	printf("%d", Mul(a + b, b + c));
+//	return 0;
+//}
+//
+//#define f 3
+//#define a 10
+//void foo();
+//int main()
+//{
+//	printf("%d..", a);
+//	foo();
+//	printf("%d", a);
+//	return 0;
+//} void foo() {
+//#undef a
+//#define a 50
+//}
+//#define N 2
+//#define M N + 1
+//#define NUM (M + 1) * M / 2//(2+1+1)*2+1/2
+//int main()
+//{
+//	printf("%d\n", NUM);
+//	return 0;
+//}
+//int i;
+//void prt()
+//{
+//	for (i = 5; i < 8; i++)
+//		printf("%c", '*');
+//	printf("\t");
+//}
+//print(char* s)
+//{
+//	if (*s)
+//	{
+//		print(++s);
+//		printf("%c", *s);
+//	}
+//}
+//int main()
+//{
+	//for (i = 5; i <= 8; i++)
+	//	prt();
+	//return 0;
+//	char ch[80] = "123abcdEFG*&";
+//	int j;
+//	puts(ch);
+//	for (j = 0; ch[j] != '\0'; j++)
+//		if (ch[j] >= 'A' && ch[j] <= 'Z')
+//			ch[j] = ch[j] + 'e' - 'E';
+//	puts(ch);
+//	return 0;
+//}
+
+//char str[] = "Geneius";
+//print(str);
+//return 0;
+//} 
